446a5febee49f0117a11ee36aabdbbfa5745779e
kuberseth002 <kuber0804.be21@chitkara.edu.in>
2024-12-02 15:31:46 +0530

    changes done

--- Class_Work/problem1.js
+++ Class_Work/problem1.js
@@ -1,12 +1,21 @@
-function ShoppingCart(){
-  let items=["apple","oranges","banana"]
-  items.push("rose")
-  console.log(items);
-   
-}
-ShoppingCart()
+let cart=[];
 
-
-function viewrprice(){
-  
+function addItems(itemName,itemPrice)
+{
+    let obj={
+        name:itemName,
+        price:itemPrice
+    }
+    cart.push(obj);
 }
+addItems("Laptop",999);
+addItems("mobile",9939);
+console.log(cart);
+
+let totalprice=cart.reduce((total,obj)=>
+{
+    return total+=obj.price;
+},0
+)
+
+console.log(totalprice)
